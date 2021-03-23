c167c17395fa75b7aa59c31efa4101c376f52453
markegor <markegoor@gmail.com>
2021-03-23 19:58:56 +0200

    Header Mobile - News Bug

--- wp-content/themes/energetik/header.php
+++ wp-content/themes/energetik/header.php
@@ -9,7 +9,7 @@
                 <div class="header-top-contact col-6 col-lg-3 d-flex flex-row flex-lg-column justify-content-end justify-content-lg-evenly">
                     <div id="header-top-contact-email" class="header-top-contact-item text-center">
                         <div class="header-top-contact-imgs d-flex justify-content-center">
-                            <a href="<?php echo PermaLinks("email")["value"] ?>"><img src="<?php echo PermaLinks("email")["icon"] ?>" /></a>
+                            <a href="mailto:<?php echo PermaLinks("email")["value"] ?>"><img src="<?php echo PermaLinks("email")["icon"] ?>" /></a>
                         </div>
                         <a href="mailto:<?php echo PermaLinks("email")["value"] ?>"><?php echo PermaLinks("email")["value"] ?></a>
                     </div>
@@ -28,15 +28,23 @@
         </div>
     </div>
     <div class="header-bottom">
-        <div class="container header-bottom-content d-flex justify-content-evenly">
+        <div class="container header-bottom-content d-flex justify-content-start justify-content-lg-evenly">
             <a class="header-bottom-item d-none d-lg-block btn btn-light btn-sm<?php if(is_front_page()) echo " current" ?>" href="/">О нас</a> 
             <a class="header-bottom-item d-none d-lg-block btn btn-light btn-sm<?php if(is_page("news")) echo " current" ?>" href="/news">Новости</a>
             <a class="header-bottom-item d-none d-lg-block btn btn-light btn-sm<?php if(is_page("portfolio")) echo " current" ?>" href="/portfolio">Портфолио</a> 
             <a class="header-bottom-item d-none d-lg-block btn btn-light btn-sm<?php if(is_page("contacts")) echo " current" ?>" href="/contacts">Контакты</a> 
-            <div id="header-bottom-item-mob-btn" class="header-bottom-item d-block d-lg-none btn btn-light btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                </svg>
+            <div class="dropdown">
+                <div class="header-bottom-item d-block d-lg-none btn btn-light btn-sm" id="dropdown-menu" data-bs-toggle="dropdown" aria-expanded="false">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                </div>
+                <ul class="dropdown-menu" aria-labelledby="dropdown-menu">
+                    <li><a class="dropdown-item py-2<?php if(is_front_page()) echo " fw-bold" ?>" href="/">О нас</a></li>
+                    <li><a class="dropdown-item py-2<?php if(is_page("news")) echo " fw-bold" ?>" href="/news">Новости</a></li>
+                    <li><a class="dropdown-item py-2<?php if(is_page("portfolio")) echo " fw-bold" ?>" href="/portfolio">Портфолио</a></li>
+                    <li><a class="dropdown-item py-2<?php if(is_page("contacts")) echo " fw-bold" ?>" href="/contacts">Контакты</a></li>
+                </ul>
             </div>
         </div>
     </div>
